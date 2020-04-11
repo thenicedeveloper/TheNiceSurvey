2000a0387033a7adbf0c89d9206236aca3125d0a
Donald Zarrillo Jr <donzarrillo@gmail.com>
2020-04-11 14:37:20 -0400

    implemented google oauth & passport.js on backend

--- config/config.js
+++ config/config.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     cookieKey: process.env.cookieKey,
-    mongoURI: process.env.mongoURI
+    mongoURI: process.env.mongoURI,
+    google_id: process.env.google_client_id,
+    google_client_secret: process.env.google_client_secret
 }
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
-const path          = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const config = require("./config/config");
-const app = express();
+
+// Mongodb schema for users
+require("./models/User");
+require("./models/Survey");
+
+// since we are not returning anything from passport we can condence it to just require.
+require("./config/passport");
 
 (async () =>{
     try {
@@ -16,8 +21,10 @@ const app = express();
     }
 })()
 
-require("./models/Survey");
-
+const app = express();
+// middleware Making it available on req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //Static files
 app.use(express.static('client/build'));
 
@@ -28,13 +35,12 @@ app.use(
         keys: [config.cookieKey]
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Define routes
+require("./routes/loginRoutes")(app);
+app.use('/api/surveys', require('./routes/surveyRoutes'));
 
-
-//Define routes
-app.use('/api/surveys', require('./routes/surveyRoutes'))
-
-
-
-const PORT          = process.env.PORT || 5000;
-app.listen(PORT, ()=>  console.log(`Server started on ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
